a01a62b3dbad1beffe7cdec734e7aed3d37f9f61
unknown <themonstermora@gmail.com>
2019-08-01 15:39:40 -0500

    commit 1

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace formularios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.grid = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumb = new System.Windows.Forms.TextBox();
+            this.txtBuildingName = new System.Windows.Forms.TextBox();
+            this.butLoad = new System.Windows.Forms.Button();
+            this.butAddField = new System.Windows.Forms.Button();
+            this.butSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.grid.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.grid);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.butLoad);
+            this.splitContainer1.Panel2.Controls.Add(this.butAddField);
+            this.splitContainer1.Panel2.Controls.Add(this.butSave);
+            this.splitContainer1.Size = new System.Drawing.Size(296, 402);
+            this.splitContainer1.SplitterDistance = 334;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // grid
+            // 
+            this.grid.Controls.Add(this.label1);
+            this.grid.Controls.Add(this.label2);
+            this.grid.Controls.Add(this.txtNumb);
+            this.grid.Controls.Add(this.txtBuildingName);
+            this.grid.Location = new System.Drawing.Point(4, 4);
+            this.grid.Name = "grid";
+            this.grid.Size = new System.Drawing.Size(292, 327);
+            this.grid.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Edificio";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero de grifos";
+            // 
+            // txtNumb
+            // 
+            this.txtNumb.Location = new System.Drawing.Point(55, 22);
+            this.txtNumb.Name = "txtNumb";
+            this.txtNumb.Size = new System.Drawing.Size(55, 20);
+            this.txtNumb.TabIndex = 3;
+            // 
+            // txtBuildingName
+            // 
+            this.txtBuildingName.Location = new System.Drawing.Point(220, 22);
+            this.txtBuildingName.Name = "txtBuildingName";
+            this.txtBuildingName.Size = new System.Drawing.Size(60, 20);
+            this.txtBuildingName.TabIndex = 2;
+            // 
+            // butLoad
+            // 
+            this.butLoad.Location = new System.Drawing.Point(209, 29);
+            this.butLoad.Name = "butLoad";
+            this.butLoad.Size = new System.Drawing.Size(75, 23);
+            this.butLoad.TabIndex = 2;
+            this.butLoad.Text = "Cargar";
+            this.butLoad.UseVisualStyleBackColor = true;
+            this.butLoad.Click += new System.EventHandler(this.ButLoad_Click);
+            // 
+            // butAddField
+            // 
+            this.butAddField.Location = new System.Drawing.Point(102, 29);
+            this.butAddField.Name = "butAddField";
+            this.butAddField.Size = new System.Drawing.Size(92, 23);
+            this.butAddField.TabIndex = 1;
+            this.butAddField.Text = "Agregar Campo";
+            this.butAddField.UseVisualStyleBackColor = true;
+            this.butAddField.Click += new System.EventHandler(this.ButAddField_Click);
+            // 
+            // butSave
+            // 
+            this.butSave.Location = new System.Drawing.Point(12, 29);
+            this.butSave.Name = "butSave";
+            this.butSave.Size = new System.Drawing.Size(75, 23);
+            this.butSave.TabIndex = 0;
+            this.butSave.Text = "Guardar";
+            this.butSave.UseVisualStyleBackColor = true;
+            this.butSave.Click += new System.EventHandler(this.ButSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 402);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.grid.ResumeLayout(false);
+            this.grid.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button butLoad;
+        private System.Windows.Forms.Button butAddField;
+        private System.Windows.Forms.Button butSave;
+        private System.Windows.Forms.Panel grid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumb;
+        private System.Windows.Forms.TextBox txtBuildingName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace formularios
+{
+
+    public partial class Form1 : Form
+    {
+        int x1 = 0;
+        int x2 = 0;
+
+        int y1 = 0;
+        int y2 = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            x1 = txtBuildingName.Location.X;
+            y1 = txtBuildingName.Location.Y;
+
+            x2 = txtNumb.Location.X;
+            y2 = txtNumb.Location.Y;
+        }
+
+        private void ButSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("C:/Users/c/Desktop/C#/formularios/formularios/datos/datos.txt", true);
+                string line = "";
+                int h = 1;
+                foreach (var txtBox in grid.Controls.OfType<TextBox>()){
+                    if (h % 2 == 0) {
+                        line += txtBox.Text;
+                        sw.WriteLine(line);
+                        line = "";
+                        h = 1;
+                    }
+                    else
+                    {
+                        line += txtBox.Text+"_";
+                        h++;
+                    }
+                }
+                sw.Close();
+            }
+            catch (Exception g)
+            {
+                MessageBox.Show(g.Message);
+            }
+            finally
+            {
+
+            }
+        }
+
+        private void ButAddField_Click(object sender, EventArgs e)
+        {
+            y1 += 25;
+            y2 += 25;
+
+            Size _size = new Size(txtBuildingName.Width, txtBuildingName.Height);
+            Size _size2 = new Size(txtNumb.Width, txtNumb.Height);
+
+            var labBuilding = new Label();
+            labBuilding.Text = "Edificio";
+            labBuilding.Location = new Point(label1.Location.X, y1);
+
+            var labNumber = new Label();
+            labNumber.Text = "Numero de grifos";
+            labNumber.Location = new Point(label2.Location.X, y1);
+
+            var txtNameB = new TextBox();
+            txtNameB.Size = _size;
+            txtNameB.Location = new Point(x1, y1);
+
+            var txtGrifN = new TextBox();
+            txtGrifN.Size = _size2;
+            txtGrifN.Location = new Point(x2, y2);
+
+            grid.Controls.Add(txtGrifN);
+            grid.Controls.Add(txtNameB);
+            grid.Controls.Add(labNumber);
+            grid.Controls.Add(labBuilding);
+        }
+
+        private void ButLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("C:/Users/c/Desktop/C#/formularios/formularios/datos/datos.txt");
+                string line = null;
+                string tot = null;
+                int c = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    ButAddField_Click(sender, e);
+                    tot += line + ":";
+                    c++;
+                }
+                string[] fields = tot.Split(':');
+                ArrayList singles = new ArrayList();
+                for (int i = 0; i < fields.Length-1; i++)
+                {
+                    string a = fields[i];
+                    string[] wea = a.Split('_');
+                    singles.Add(wea[0]);
+                    singles.Add(wea[1]);
+                }
+                foreach (string a in singles)
+                {
+                    Console.WriteLine(a);
+                }
+                int h = 0;
+                foreach (var txtBox in grid.Controls.OfType<TextBox>())
+                {
+                    if (h < singles.Count)
+                    {
+                        txtBox.Text = (string)singles[h];
+                        h++;
+                    }
+                }
+                    sr.Close();
+            }
+            catch (Exception g)
+            {
+                MessageBox.Show(g.Message);
+            }
+            finally
+            {
+            }
+        }
+
+    }
+}
